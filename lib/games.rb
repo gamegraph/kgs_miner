2c250f63fa8865472e848dd69f00acf7b732cc04
Jared Beck <jared@jaredbeck.com>
2013-06-29 23:10:52 -0400

    relax range in `expected_num_uniq_players`

--- lib/games.rb
+++ lib/games.rb
@@ -17,11 +17,12 @@ module KgsMiner
       end
     end
 
-    # Given a set of G games between me and everyone else, the max.
-    # number of unique players is G + 1.  The minimum, if I play
-    # only one other person, is 2.
+    # KGS usernames seem to be case-insensitive.  However, a player
+    # can change the capitalization of their account somehow. See
+    # http://www.gokgs.com/gameArchives.jsp?user=Alf&oldAccounts=t&year=2012&month=12
+    # Therefore, it's possible to find a maximum of games * 2 usernames.
     def self.expected_num_uniq_players games
-      (2..games.length + 1)
+      (2..games.length * 2)
     end
   end
 end
